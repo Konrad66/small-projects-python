96762e32f4d68215162a326d2358f4d5169722e5
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2024-11-16 21:57:39 +0100

    dekorator property

--- dekoratory_property.py
+++ dekoratory_property.py
@@ -3,27 +3,48 @@ class FuelTank:
     def __init__(self):
         self.__capacity = 50
         self.__current_level = 0
-        # self.current_level = current_level
+
+    @property
+    def capacity(self) -> float:
+        return self.__capacity
+
+    @property
+    def current_level(self) -> float:
+        return self.__current_level
+
+    @current_level.setter
+    def current_level(self, new_level) -> None:
+        if 0 <= new_level <= self.__capacity:
+            self.__current_level = new_level
+        else:
+            print(f"Poziom paliwa musi być między 0 a {self.__capacity}")
 
     def fill_up(self, __amount_fuel: float) -> None:
         self.__current_level += __amount_fuel
         if self.__current_level > self.__capacity:
             self.__current_level = self.__capacity
-
-    def get_capacity(self) -> float:
-        return self.__capacity
-
-    def get_current_level(self) -> str:
-        percent: float = (self.__current_level * 100) / self.__capacity
-        return f"{percent}%"
+        else:
+            print(f"Poziom paliwa musi być między 0 a {self.__capacity}")
 
     def __str__(self) -> str:
-        return f"Bak: Pojemnosc zbiornika wynosi: {self.__capacity}, twój aktualny stan paliwa wynosi: {self.get_current_level()}"
+        percent: float = (self.__current_level * 100) / self.__capacity
+        return f"Bak: Pojemnosc zbiornika wynosi: {self.__capacity}, twój aktualny stan paliwa wynosi: {self.__current_level} co daje {percent}%"
 
 
 class Engine:
     def __init__(self):
         self.__running = False
+
+    @property
+    def running(self) -> bool:
+        return self.__running
+
+    @running.setter
+    def running(self, state) -> None:
+        if isinstance(state, bool):
+            self.__running = state
+        else:
+            raise ValueError("Running musi byc typu boolean")
 
     def turn_on(self) -> None:
         self.__running = True
@@ -31,36 +52,34 @@ class Engine:
     def turn_off(self) -> None:
         self.__running = False
 
-    def get_running_state(self) -> bool:
-        return self.__running
-
     def __str__(self) -> str:
         return f"Silnik: wlaczony {self.__running}"
 
 
 class Auto:
-    # pass  # -> piszemy jeżeli chcemy aby klasa przez jakiś czas była pusta
-    # , dotyczy to metod, klass (ciało po : z wcięciem )
     def __init__(self):
         self.__engine = Engine()
         self.__tank = FuelTank()
 
-    def get_engine(self) -> Engine:
+    @property
+    def engine(self) -> Engine:
         return self.__engine
 
-    def get_fuel_tank(self) -> FuelTank:
+    @property
+    def fuel_tank(self) -> FuelTank:
         return self.__tank
 
-    def set_engine(self, engine: Engine) -> None:
+    @engine.setter
+    def engine(self, engine: Engine) -> None:
         self.__engine = engine
 
-    def set_fuel_tank(self, tank: FuelTank) -> None:
+    @fuel_tank.setter
+    def fuel_tank(self, tank: FuelTank) -> None:
         self.__tank = tank
+
 
     def fill_up(self, amount_fuel: float) -> None:
         self.__tank.fill_up(amount_fuel)
-
-    # todo zrobic wszystkie pomocnicze metody
 
     def turn_on_engine(self) -> None:
         self.__engine.turn_on()
@@ -71,22 +90,13 @@ class Auto:
     def info(self) -> str:
         return f"Auto: {self.__engine}, {self.__tank}"
 
-    # todo lepsze bo nie trzeba jawnie tego wywoływać
     def __str__(self) -> str:
         return f"Auto: {self.__engine}, {self.__tank}"
 
 
-# my_fuel_tank: FuelTank = FuelTank()
-# # todo pokazać w taki sposób bez użycia toStringa
-# # print(fuel_tank.capacity)
-# my_engine: Engine = Engine()
 my_auto: Auto = Auto()
-# my_fuel_tank.fill_up(15)
-# my_engine.turn_off()
-# print(my_fuel_tank)
-# print(my_engine)
-# print(my_auto)
-
+my_auto.get_fuel_tank().fill_up(50)
+print(my_auto.get_fuel_tank().current_level)
 print(my_auto.get_engine())
 print(my_auto.info())
 my_auto.turn_off_engine()
